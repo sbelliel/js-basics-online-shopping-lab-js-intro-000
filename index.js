1e5288c4e75b973a7ccac6797a81222175423412
Sam Belliel <sbelliel@gmail.com>
2018-08-13 22:23:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,13 +19,13 @@ return item + " has been added to your cart.";
 }
 
 function viewCart() {
-  if (cart.length < 1){
+  if (getCart().length < 1){
   return "Your shopping cart is empty."
-} else if (cart.length = 1) {
+} else if (getCart().length = 1) {
   return "In your cart, you have " + cart[0].itemName + " at $" + cart[0].itemPrice + "."
 
-} else if (cart.length = 2) {
-  return "In your cart, you have " + cart[0].itemName + " at $" + cart[0].itemPrice + ", and " + cart[1].itemName + " at $" + cart[1].itemPrice + "."
+} else if (getCart().length = 2) {
+  return "In your cart, you have " + getCart()[0].itemName + " at $" + getCart()[0].itemPrice + ", and " + getCart()[1].itemName + " at $" + getCart()[1].itemPrice + "."
 
 } else {
   return "In your cart, you have " + cart[0].itemName + " at $" + cart[0].itemPrice + ", " + cart[1].itemName + " at $" + cart[1].itemPrice + "," + " and " + cart[2].itemName + " at $" + cart[2].itemPrice + "."
